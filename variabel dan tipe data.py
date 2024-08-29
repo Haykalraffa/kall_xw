a5dfe168ae506ecd6a0bed3021bd998af78464d2
Haykalraffa <haykalrafazulkarnaen@gmail.com>
2024-08-29 12:44:44 +0700

    Update variabel dan tipe data.py

---
+++ variabel dan tipe data.py
@@ -0,0 +1,20 @@
+nama = "haykal rafa"
+umur = 17
+tinggi = 1.65
+
+harga = 15000 #tipe int
+barat = 33.12 #float
+jarak = 3e3 #float 3000.0
+
+nama = "haykal"
+jenis_kelamin = 'L'
+alamat = """
+Jl. pramuka, No 32. RT 04,
+ciamis, Jawabarat
+"""
+agama = 'islam'
+
+print("haykal : ", nama)
+print("ciamis : ", alamat)
+print("17 : ", umur)
+print("1.65 : ", tinggi)
